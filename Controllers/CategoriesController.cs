3d50b84f71b3c67a5d2c2b290d58babf3969b7c5
SajalSanthosh <61447187+sajalsanthosh@users.noreply.github.com>
2020-10-01 15:26:13 -0400

    Add project files.

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult details(string category)
+        {
+            ViewBag.message = "You selected category: " + category;
+            return View();
+        }
+    }
+}
